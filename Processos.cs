e2e6b4411b97dcdfc0796608240060009de95cd1
Venzke <67403605+Venzke@users.noreply.github.com>
2020-11-03 15:15:50 -0300

    Add files via upload

---
+++ Processos.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+
+namespace Exercicio4_ClassMethod_C_Sharp
+{
+    class Processos
+    {
+        public string Nome;
+        public float Nota1, Nota2, Nota3;
+
+        public float Nota_Final()
+        {
+            return Nota1 + Nota2 + Nota3;
+        }
+
+        public string Situacao()
+        {
+
+            double result = Nota1 + Nota2 + Nota3;
+
+            if(result >= 60)
+            {
+                return "Aprovado";
+            }
+            else
+            {
+                return "Reprovado";
+            }
+
+        }
+
+        public float Nota_Faltam()
+        {
+
+            return 60 - (Nota1 + Nota2 + Nota3);
+
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+
+namespace Exercicio4_ClassMethod_C_Sharp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Processos p = new Processos();
+
+            Console.Write("Nome do aluno: ");
+            p.Nome = Console.ReadLine();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Insira as notas do aluno.");
+            Console.Write("Nota 1: ");
+            p.Nota1 = float.Parse(Console.ReadLine());
+            
+            Console.Write("Nota 2: ");
+            p.Nota2 = float.Parse(Console.ReadLine());
+
+            Console.Write("Nota 3: ");
+            p.Nota3 = float.Parse(Console.ReadLine());
+
+            
+            double Nota_Max = p.Nota_Final();
+            Console.WriteLine("Nota final: {0}", Nota_Max);
+
+            string Resultado = p.Situacao();
+
+            if (Resultado == "Reprovado")
+            {
+                Console.WriteLine();
+                Console.WriteLine("{0}", Resultado);
+
+                float Notas_Faltam = p.Nota_Faltam();
+
+                Console.WriteLine("Faltaram {0} pontos.", Notas_Faltam);
+
+            }
+            else
+            {
+                Console.WriteLine("{0}", Resultado);
+            }
+
+
+            Console.Read();
+        }
+    }
+}
